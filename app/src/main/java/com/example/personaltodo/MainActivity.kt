d35cfe4e8be9b4457f41bc66127028fb500cdfb0
Leonardo Rodrigues <leonardo.rodrigues@poatek.com>
2023-10-06 12:23:32 -0300

    Add AppCenter SDK

--- app/src/main/java/com/example/personaltodo/MainActivity.kt
+++ app/src/main/java/com/example/personaltodo/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.personaltodo
 
 import android.os.Bundle
-import android.window.OnBackInvokedDispatcher
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,6 +10,9 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.remember
 import com.example.personaltodo.featureevents.presentation.ui.view.EventListComponent
 import com.example.personaltodo.ui.theme.PersonalToDoTheme
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,6 +20,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val appSecret = "dbe8aa27-777b-4df0-8ea3-d13e85567366"
+
+        AppCenter.start(
+            application,
+            appSecret,
+            Analytics::class.java,
+            Crashes::class.java
+        )
+
         setContent {
             PersonalToDoTheme {
                 val snackbarHostState = remember { SnackbarHostState() }
